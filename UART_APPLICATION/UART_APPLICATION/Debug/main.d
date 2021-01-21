d86aa4753415a9406a72a0efbc063a2304d0105a
Ahmed Yousri <66730765+AhmedYousriSobhi@users.noreply.github.com>
2021-01-21 19:20:42 +0200

    Add files via upload

---
+++ UART_APPLICATION/UART_APPLICATION/Debug/main.d
@@ -0,0 +1,34 @@
+main.d main.o: .././main.c \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Setup\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom16a.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Setup\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom16a.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\setup\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
---
+++ UART_APPLICATION/UART_APPLICATION/main.c
@@ -0,0 +1,54 @@
+/*
+ * UART_APPLICATION.c
+ *
+ * Created: 12/30/2020 4:15:06 PM
+ * Author : kidna
+ */ 
+
+#include <avr/io.h>
+
+void USART_Init( unsigned int baud );
+void USART_Transmit( unsigned char data );
+void USART_Transmit_Msg(uint8_t *word );
+uint8_t messege[]= "HELLO FROM APPLICATION\n";
+
+int main(void)
+{
+    USART_Init(12); // setting baudrate to 4800
+    while (1) 
+    {
+		USART_Transmit_Msg((uint8_t *)"HELLO FROM APPLICATION\n");
+		USART_Transmit_Msg((uint8_t *)"YOU HAVE DONE YOUR FIRST BOOTLOADER CODE\n");
+    }
+}
+
+void USART_Init( uint16_t baud )
+{
+	/* Set baud rate */
+	UBRRH = (unsigned char)(baud>>8);
+	UBRRL = (unsigned char)baud;
+	/* Enable receiver and transmitter */
+	UCSRB = (1<<RXEN)|(1<<TXEN);
+	/* Set frame format: 8data, 1stop bit */
+	UCSRC = (1<<URSEL)|(3<<UCSZ0);
+}
+
+void USART_Transmit( uint8_t data )
+{
+	/* Wait for empty transmit buffer */
+	while ( !( UCSRA & (1<<UDRE)) )
+	;
+	/* Put data into buffer, sends the data */
+	UDR = data;
+}
+
+void USART_Transmit_Msg(uint8_t *word )
+{
+	uint8_t index = 0;
+	while(word[index] != '\n')
+	{
+		USART_Transmit(word[index++]);
+	}
+	USART_Transmit('\n');
+	
+}
